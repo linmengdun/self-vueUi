c4e8631056b53033a2240fb603f314e2b1ed514f
浪小白 <langxiaobai2814@ewan.cn>
2023-09-28 12:02:12 +0800

    feat:添加pm2配置文件

---
+++ ecosystem.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    apps: [
+        {
+            name: 'my-vue-app',
+            script: 'node_modules/@vue/cli-service/bin/vue-cli-service.js',
+            args: 'serve',
+            env: {
+                NODE_ENV: 'development',
+                VUE_APP_CLI_UI_URL: 'ws://localhost:4030/graphql',
+                VUE_APP_GRAPHQL_PORT: '4030'
+            }
+        },
+        {
+            name: 'apollo',
+            script: 'node_modules/@vue/cli-service/bin/vue-cli-service.js',
+            args: 'apollo:dev',
+            env: {
+                VUE_APP_CLI_UI_DEV: true,
+                VUE_APP_CLI_UI_DEBUG: true,
+                VUE_APP_GRAPHQL_PORT: '4030'
+            }
+        }
+    ]
+};
